a60383ac52ce60534264d8550689a1bf314723ed
PengXiao <eagle.xiao@gmail.com>
2019-12-11 07:00:22 +1100

    Moved diagram to Workspace.

---
+++ src/components/Workspace.vue
@@ -0,0 +1,32 @@
+<template>
+  <sequence-diagram>{{dsl}}</sequence-diagram>
+</template>
+
+<script>
+  export default {
+  name: 'Workspace',
+  data: function() {
+    return {
+      dsl: 'A.method()'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
