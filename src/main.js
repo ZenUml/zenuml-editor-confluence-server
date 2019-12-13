ae0d89718e2cb25b809ea0e57834145e861ac870
PengXiao <eagle.xiao@gmail.com>
2019-12-13 16:03:23 +1100

    Removed 'sequence-diagram' as web component. Binding dsl as <sequence-diagram>{{dsl}}</sequence-diagram> does not work.

--- src/main.js
+++ src/main.js
@@ -10,12 +10,6 @@ import App from './App.vue'
 Vue.use(Vuex)
 Vue.use(VueCodeMirror)
 
-/*
- * According to ES6, `import` statements are always hoisted to the top of the file. This is a workaround to make sure
- * that Vue is exposed before injecting `sequence-diagram` web component
-*/
-window.Vue = Vue
-require('sequence-diagram')
 Vue.config.productionTip = false
 const store = new Vuex.Store(Store);
 
